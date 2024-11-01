a7839d49c2efb32ee91af820f6341e48af601cc4
Nicholas Eddy <nicholas.g.eddy@gmail.com>
2024-11-01 12:44:44 -0400

    cart endpoints completed

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+   getCartProducts,
+   addToCart,
+   updateQuantity,
+   removeAllFromCart,
+} from '../controllers/cart.controller.js';
+
+import { protectRoute } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+router.get('/', protectRoute, getCartProducts);
+router.post('/', protectRoute, addToCart);
+router.put('/:id', protectRoute, updateQuantity);
+router.delete('/', protectRoute, removeAllFromCart);
+
+
+export default router;
